8e7845554f5744eb2466a08f644d83871d7fdc65
vishakha <Vishw@DESKTOP-SMSDJR0>
2022-02-06 14:52:29 +0530

    Assignment9

---
+++ Inner classes/src/AnonymousInnerClass.java
@@ -0,0 +1,5 @@
+abstract class AnonymousInnerClass {
+	public abstract void display();
+
+	protected abstract void display1();
+}
---
+++ Inner classes/src/InnerClassAssisted3.java
@@ -0,0 +1,22 @@
+
+public class InnerClassAssisted3 {
+
+	public static void main(String[] args) {
+		AnonymousInnerClass i = new AnonymousInnerClass() {
+
+			public void display() {
+				System.out.println("Anonymous Inner Class");
+			}
+
+			protected void display1() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+		i.display();
+	}
+}
+
+
+
+
