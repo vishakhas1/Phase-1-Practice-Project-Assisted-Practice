92a6fdc47159d19a07ce1c2cf418af52d82ef822
vishakha <Vishw@DESKTOP-SMSDJR0>
2022-02-07 17:25:33 +0530

    Assignment18

---
+++ Exception Handling/src/com/mphasis/ExceptionHandling.java
@@ -0,0 +1,27 @@
+package com.mphasis;
+
+@SuppressWarnings("serial")
+class MyException extends Exception {
+	String str1;
+
+	MyException(String str2) {
+		str1 = str2;
+	}
+
+	public String toString() {
+		return ("MyException Occurred: " + str1);
+	}
+}
+
+class Example1 {
+	public static void main(String args[]) {
+		try {
+			System.out.println("Starting of try block");
+			// I'm throwing the custom exception using throw
+			throw new MyException("This is My error Message");
+		} catch (MyException exp) {
+			System.out.println("This is catch Block");
+			System.out.println(exp);
+		}
+	}
+}
