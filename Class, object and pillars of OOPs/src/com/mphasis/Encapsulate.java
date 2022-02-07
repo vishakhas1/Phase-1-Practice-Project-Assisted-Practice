b734d4e99f4ebc8578f202d6bc8e613571231c4e
vishakha <Vishw@DESKTOP-SMSDJR0>
2022-02-07 18:10:46 +0530

    Assignment20

---
+++ Class, object and pillars of OOPs/src/com/mphasis/Encapsulate.java
@@ -0,0 +1,33 @@
+package com.mphasis;
+
+public class Encapsulate 
+{ 
+    private String Name; 
+    private int Roll; 
+    private int Age;
+    public int getAge()  
+    { 
+      return Age; 
+    } 
+    public String getName()  
+    { 
+      return Name; 
+    } 
+    public int getRoll()  
+    { 
+       return Roll; 
+    } 
+    public void setAge( int newAge) 
+    { 
+      Age = newAge; 
+    } 
+    public void setName(String newName) 
+    { 
+      Name = newName; 
+    } 
+    public void setRoll( int newRoll)  
+    { 
+      Roll = newRoll; 
+    } 
+}
+
